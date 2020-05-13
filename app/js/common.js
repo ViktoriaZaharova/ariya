c967fcae7f294d5554d9deaf0bf634e0b59ce3ec
viktoriazaharova <zaharova181114@gmail.com>
2020-05-13 14:54:31 +0300

    slide

--- app/js/common.js
+++ app/js/common.js
@@ -6,3 +6,10 @@ $('.btn-close').click(function () {
    $('.mobile-menu').fadeOut();
 });
 
+$('main').slick({
+   arrows: false,
+   fade: true,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   adaptiveHeight: true
+});
